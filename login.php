a7354c2028aa2f8496109181828bec5b1fe9efa0
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-10-05 21:19:20 +0700

    edit line

--- login.php
+++ login.php
@@ -1,21 +1,22 @@
 <?php
-   session_start();
-   require_once("config.php");
-   $user = $_POST["username"];
-   $pass = $_POST["password"];
-   $sql = "SELECT * FROM user WHERE username = '$user' ";
-   $query = $db->query($sql);
-   $hasil = $query->fetch_assoc();
-   if($query->num_rows == 0) {
-     echo '<div class="alert alert-danger">*Username belum terdaftar <a href="index" >Kembali</a> </div>'; 
-   } else {
-     if($pass <> $hasil['password']) {
-       echo '<div class="alert alert-danger">*Password Salah <a href="index" >Kembali</a></div>'; 
-     } else {
-       $_SESSION['username'] = $hasil['username'];
-       header('location:halo/');
-       $_SESSION['password'] = $hasil['password'];
-       header('location:halo/');
-     }
-   }
+session_start();
+require_once "config.php";
+$user = $_POST["username"];
+$pass = $_POST["password"];
+$sql = "SELECT * FROM user WHERE username = '$user' ";
+$query = $db->query($sql);
+$hasil = $query->fetch_assoc();
+if ($query->num_rows == 0) {
+    echo '<div class="alert alert-danger">*Username belum terdaftar <a href="index" >Kembali</a> </div>';
+} else {
+    if ($pass != $hasil["password"]) {
+        echo '<div class="alert alert-danger">*Password Salah <a href="index" >Kembali</a></div>';
+    } else {
+        $_SESSION["username"] = $hasil["username"];
+        header("location:halo/");
+        $_SESSION["password"] = $hasil["password"];
+        header("location:halo/");
+    }
+}
 ?>
+
--- register.php
+++ register.php
@@ -1,30 +1,31 @@
 <?php
-    require "config.php";
-    require "vigenere/encrypt.php";
-    $sql = "SELECT * FROM user WHERE username = '$user' ";
-    $query = $db->query($sql);
-    if($query->num_rows != 0) {
-        echo "<div align='center'>username Sudah Terdaftar! <a href='index'>Back</a></div>";
-    } else {
-        if(!$user || !$pass || !$key) {
+require "config.php";
+require "cipher/encrypt.php";
+$sql = "SELECT * FROM user WHERE username = '$user' ";
+$query = $db->query($sql);
+if ($query->num_rows != 0) {
+    echo "<div align='center'>username Sudah Terdaftar! <a href='index'>Back</a></div>";
+} else {
+    if (!$user || !$pass || !$key) {
         echo "<div align='center'>Masih ada data yang kosong! <a href='index'>Back</a>";
-        } else {
+    } else {
         $data = "INSERT INTO user VALUES (NULL, '$user', '$pass', '$key')";
         $simpan = $db->query($data);
-        if($simpan) {
+        if ($simpan) {
             echo "<div align='center'>Pendaftaran Sukses, Silahkan <a href='index'>Login</a></div>";
         } else {
             echo "<div align='center'>Proses Gagal!</div>";
         }
-        }
     }
-    if(isset($user)) {
-        echo "Key: <b>". $key . "</b><br>";
-        echo "Username: <b>". $username ."</b><br>";
-        echo "Encripted Text: <b>". $user ."</b><br>"; 
-    }
-    if(isset($pass)) {
-        echo "Password: <b>". $password ."</b><br>";
-        echo "Encripted Text: <b>". $pass ."</b><br>"; 
-    }
+}
+if (isset($user)) {
+    echo "Key: <b>" . $key . "</b><br>";
+    echo "Username: <b>" . $username . "</b><br>";
+    echo "Encripted Text: <b>" . $user . "</b><br>";
+}
+if (isset($pass)) {
+    echo "Password: <b>" . $password . "</b><br>";
+    echo "Encripted Text: <b>" . $pass . "</b><br>";
+}
 ?>
+
